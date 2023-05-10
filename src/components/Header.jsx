7d2cf75661f300192b75190c38be85bcb3d2c391
rahul01-git <rahulneupane0090@gmail.com>
2023-05-11 00:54:30 +0545

    dark mode integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export const Header = () => {
   const activeClass =
     "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
   const inActiveClass =
     "text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
   const [hidden, setHidden] = useState(true);
+  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) || false)
+  useEffect(()=>{
+    localStorage.setItem("darkMode",JSON.stringify(darkMode))
+
+    darkMode ? document.documentElement.classList.add("dark") : document.documentElement.classList.remove("dark")
+
+  },[darkMode])
   return (
     <header>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 border-b-2 dark:border-b-1 dark:border-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center">
             <img src={Logo} className="h-8 mr-3" alt="Cinemate Logo" />
@@ -17,7 +24,11 @@ export const Header = () => {
               Cinemate
             </span>
           </Link>
-          <div className="flex md:order-2">
+
+          <div id="mobile-nav" className="flex md:order-2">
+          <button onClick={() => setDarkMode(!darkMode)} data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip" type="button" data-toggle-dark="light" className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+            { darkMode ? (<svg aria-hidden="true" data-toggle-icon="sun" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path></svg>) : (<svg aria-hidden="true" data-toggle-icon="moon" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>) }
+          </button>
             <button
               type="button"
               data-collapse-toggle="navbar-search"
@@ -90,9 +101,10 @@ export const Header = () => {
               </svg>
             </button>
           </div>
+
           <div
             className={`items-center justify-between ${hidden? "hidden" : ""} w-full md:flex md:w-auto md:order-1`}
-            id="navbar-search"
+            id="navbar-search nav-links"
           >
             <div className="relative mt-3 md:hidden">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -162,6 +174,7 @@ export const Header = () => {
               </li>
             </ul>
           </div>
+
         </div>
       </nav>
     </header>
