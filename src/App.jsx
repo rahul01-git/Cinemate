8731eb9363c773efe430182bdcc79ca79e67fd8f
rahul01-git <rahulneupane0090@gmail.com>
2023-05-11 00:14:34 +0545

    header and footer implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { AllRoutes } from "./routes/AllRoutes"
+import { Header , Footer } from "./components"
 export default function App() {
   return (
     <div className="App">
+      <Header/>
       <AllRoutes/>
+      <Footer/>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="bg-white shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2030{" "}
+          <Link href="/" className="hover:underline">
+            Cinemate™
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" className="hover:underline">
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
